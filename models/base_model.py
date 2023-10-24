6607ce6a8e426ff0ca6f628dda31d544bd145a38
Adrian <dreamweaver3d@gmail.com>
2023-10-24 13:36:07 +0800

    airbnb1 Task 3 commit 1

---
+++ models/base_model.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+""" Nameless module for BaseModel class """
+import json
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """A BaseModel class.
+
+    Args:
+        None
+
+    Parameters:
+        id (str): contains the uuid for the model
+        created_at (datetime): created datetime
+        updated_at (datetime): last updated datetime
+
+    Returns:
+        Nothing
+    """
+
+    id = None
+    created_at = None
+    updated_at = None
+
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        return "[BaseModel] ({0}) {1}".format(self.id, self.__dict__)
+
+    def save(self):
+        """Updates updated_at with current datetime
+
+        Args:
+            None
+
+        Returns:
+            Nothing
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Return dictionary of all key value pairs of this instance
+
+        Args:
+            None
+
+        Returns:
+            Nothing
+        """
+        new_dict = self.__dict__.copy()
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
+        new_dict["__class__"] = self.__class__.__name__
+
+        return new_dict
