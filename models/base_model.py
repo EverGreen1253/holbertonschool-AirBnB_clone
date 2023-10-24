7e9ebd361ef016ed793d6076b32be280feb1138d
Adrian <dreamweaver3d@gmail.com>
2023-10-24 18:36:47 +0800

    aibnb1 task 4 commit 1

--- models/base_model.py
+++ models/base_model.py
@@ -24,11 +24,20 @@ class BaseModel:
     created_at = None
     updated_at = None
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize class BaseModel"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+        if len(kwargs) > 0:
+            for key in kwargs:
+                if key == 'created_at':
+                    setattr(self, key, datetime.fromisoformat(kwargs[key]))
+                elif key == 'updated_at':
+                    setattr(self, key, datetime.fromisoformat(kwargs[key]))
+                elif key != '__class__':
+                    setattr(self, key, kwargs[key])
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
 
     def __str__(self):
         """Prints formatted string"""
