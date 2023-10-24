44fda84c0be47bbdc5307dcd3e435b90e6d7ede4
Adrian <dreamweaver3d@gmail.com>
2023-10-24 14:04:06 +0800

    airbnb1 Task 3 commit 7

--- models/base_model.py
+++ models/base_model.py
@@ -1,69 +1,43 @@
 #!/usr/bin/python3
-""" Nameless module for BaseModel class """
+"""Module with class BaseModel"""
+
 import json
 import uuid
 from datetime import datetime
 
 
 class BaseModel:
-    """A BaseModel class.
+    """Defines all common attributes/methods for other classes"""
 
-    Args:
-        None
-
-    Parameters:
-        id (str): contains the uuid for the model
-        created_at (datetime): created datetime
-        updated_at (datetime): last updated datetime
-
-    Returns:
-        Nothing
-    """
-
-    id = None
-    created_at = None
-    updated_at = None
-
-    def __init__(self):
-        """Initialize class BaseModel"""
+    def __init__(self, *args, **kwargs):
+        """Initialize class attibutes"""
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    v = datetime.strptime(v, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, k, v)
+                else:
+                    if k != "__class__":
+                        setattr(self, k, v)
+        else:
+            pass
 
     def __str__(self):
         """Prints formatted string"""
-        return "[BaseModel] ({0}) {1}".format(self.id, self.__dict__)
+        return (f"[{type(self).__name__}] ({self.id}) {self.to_dict()}")
 
     def save(self):
-        """Updates updated_at with current datetime
-
-        Args:
-            None
-
-        Returns:
-            Nothing
-        """
+        """Updates instance attribute updated_at with current datetime"""
         self.updated_at = datetime.now()
-        # dict_str = str(json.dumps(self.to_dict()))
-
-        # file_name = "BaseModel.json"
-
-        # with open(file_name, "w", encoding="utf-8") as f:
-        #     f.write(dict_str)
-
 
     def to_dict(self):
-        """Return dictionary of all key value pairs of this instance
-
-        Args:
-            None
-
-        Returns:
-            Nothing
-        """
+        """Returns the dictionary representation of an instance """
         new_dict = self.__dict__.copy()
         new_dict["created_at"] = self.created_at.isoformat()
         new_dict["updated_at"] = self.updated_at.isoformat()
-        new_dict["__class__"] = self.__class__.__name__
-
+        new_dict["__class__"] = type(self).__name__
         return new_dict
