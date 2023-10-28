de327ce8d7d45c5574fefe0c50468b76e92cf796
Adrian <dreamweaver3d@gmail.com>
2023-10-28 12:13:49 +0800

    airbnb1 task 8 commit 7

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -10,31 +10,30 @@ class TestBase(unittest.TestCase):
     def test_attr_email(self):
         """Tests email attribute
         """
-        user = User()
-        self.assertTrue(hasattr(user, "email"))
-        self.assertEqual(user.email, "")
+        u = User()
+        self.assertTrue(hasattr(u, "email"))
+        self.assertEqual(u.email, "")
 
     def test_attr_password(self):
         """Tests password attribute
         """
         u = User()
-        test_string = "yo_momma_is_phat_123"
-        u.password = test_string
-        self.assertEqual(type(u.password).__name__, "str")
+        self.assertTrue(hasattr(u, "password"))
+        self.assertEqual(u.password, "")
 
     def test_attr_first_name(self):
         """Tests first_name attribute
         """
         u = User()
-        u.first_name = "Clark"
-        self.assertEqual(type(u.first_name).__name__, "str")
+        self.assertTrue(hasattr(u, "first_name"))
+        self.assertEqual(u.first_name, "")
 
     def test_attr_last_name(self):
         """Tests last_name attribute
         """
         u = User()
-        u.last_name = "Kent"
-        self.assertEqual(type(u.last_name).__name__, "str")
+        self.assertTrue(hasattr(u, "last_name"))
+        self.assertEqual(u.last_name, "")
 
     def test_to_str_method(self):
         """Tests that the instance can be stringified
